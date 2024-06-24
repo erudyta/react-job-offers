6a67535a39f6c854718b48c89e41ff53658d427a
Kamil Pyszniak <x.pyszniak@gmail.com>
2024-06-24 21:37:48 +0200

    Preapring data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import OFFERS from './js/offers.js'
+
 import Layout from './Components/Pages/Layout.jsx'
 import ErrorPage from './Components/Pages/ErrorPage.jsx'
 import Offers from './Components/Pages/Offers.jsx'
@@ -10,14 +12,23 @@ const router = createBrowserRouter([
 		element: <Layout />,
 		errorElement: <ErrorPage />,
 		children: [
-			{ path: '/', element: <Offers text={'Global'}/> },
-			{ path: '/javascript', element: <Offers text={'JS'}/> },
-			{ path: '/java', element: <Offers text={'Java'}/> },
-			{ path: '/python', element: <Offers text={'Python'}/> },
-			{ path: '/cplusplus', element: <Offers text={'C++'}/> },
+			{ path: '/', element: <Offers text={'Global'} /> },
+			{ path: '/javascript', element: <Offers text={'JS'} /> },
+			{ path: '/java', element: <Offers text={'Java'} /> },
+			{ path: '/python', element: <Offers text={'Python'} /> },
+			{ path: '/cplusplus', element: <Offers text={'C++'} /> },
 		],
 	},
 ])
+
+const newOFFERS = OFFERS.map(offer => {
+	const isArr = Array.isArray(offer.lang)
+	if (isArr) {
+		return { ...offer, toFilter: [...offer.lang , ...offer.keyWords, offer.companyName] }
+	}else {
+		return { ...offer, toFilter: [offer.lang, ...offer.keyWords, offer.companyName] }
+	}
+})
 
 export default function App() {
 	return <RouterProvider router={router} />
---
+++ src/js/offers.js
@@ -0,0 +1,109 @@
+import logo1 from '../assets/logo1.png'
+import logo2 from '../assets/logo2.png'
+import logo3 from '../assets/logo3.png'
+import logo4 from '../assets/logo4.png'
+import logo5 from '../assets/logo5.png'
+import logo6 from '../assets/logo6.png'
+import logo7 from '../assets/logo7.png'
+import logo8 from '../assets/logo8.jpg'
+
+const OFFERS = [
+    {
+        id: 'offer-1',
+        img: logo1,
+        altImg: 'IT logo company',
+        companyName: 'Quest',
+        position: 'Frontend Developer',
+        publicDate: '2 weeks ago',
+        typeOfContract: 'Part Time',
+        keyWords: ['HTML', 'CSS' , 'JavaScript', 'React'],
+        place: 'Remote',
+        lang: 'JavaScript'
+    },
+    {
+        id: 'offer-2',
+        img: logo2,
+        altImg: 'IT logo company',
+        companyName: 'Epicify',
+        position: 'Junior Frontend Developer',
+        publicDate: '5 days ago',
+        typeOfContract: 'Full Time',
+        keyWords: ['Junior', 'Angular' , 'TypeScript'],
+        place: 'Seoul',
+        lang: ['JavaScript', 'TypeScript' ]
+    },
+    {
+        id: 'offer-3',
+        img: logo3,
+        altImg: 'IT logo company',
+        companyName: 'Arcadeo',
+        position: 'Backend Developer',
+        publicDate: '2 days ago',
+        typeOfContract: 'Full Time',
+        keyWords: ['Hibernate', 'Java', 'Spring Boot'],
+        place: 'Wrocław',
+        lang: 'Java'
+    },
+    {
+        id: 'offer-4',
+        img: logo4,
+        altImg: 'IT logo company',
+        companyName: 'Gamerise',
+        position: 'C++ Developer',
+        publicDate: '4 days ago',
+        typeOfContract: 'Full Time',
+        keyWords: ['C++', 'Jira' , 'Git'],
+        place: 'Poland',
+        lang: 'C++'
+    },
+    {
+        id: 'offer-5',
+        img: logo5,
+        altImg: 'IT logo company',
+        companyName: 'Pixelize',
+        position: 'Java Engineer',
+        publicDate: '1 week ago',
+        typeOfContract: 'Full Time',
+        keyWords: ['Java', 'Gradle' , 'Maven'],
+        place: 'China',
+        lang: 'Java'
+    },
+    {
+        id: 'offer-6',
+        img: logo6,
+        altImg: 'IT logo company',
+        companyName: 'Powerhouse',
+        position: 'Software Engineer',
+        publicDate: '3 days ago',
+        typeOfContract: 'Full Time',
+        keyWords: ['Python', 'SQL' , 'Git'],
+        place: 'Remote',
+        lang: 'Python'
+    },
+    {
+        id: 'offer-7',
+        img: logo7,
+        altImg: 'IT logo company',
+        companyName: 'Strategy House',
+        position: 'React Developer',
+        publicDate: '7 days ago',
+        typeOfContract: 'Full Time',
+        keyWords: ['JavaScript', 'React' , 'GitHub'],
+        place: 'Warszawa',
+        lang: 'JavaScript'
+    },
+    {
+        id: 'offer-8',
+        img: logo8,
+        altImg: 'IT logo company',
+        companyName: 'Console Champs',
+        position: 'Senior Full Stack Developer',
+        publicDate: '3 weeks ago',
+        typeOfContract: 'Part Time',
+        keyWords: ['Python', 'DevOps' , 'SQL'],
+        place: 'Remote',
+        lang: 'Python'
+    }
+]
+
+export default OFFERS
